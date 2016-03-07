15d5e258ca2fe7d21ec7fd9cb8334ebfade1d8fa
Date Huang <tjjh89017@hotmail.com>
2016-03-07 10:54:02 +0800

    update

--- hw1/main.c
+++ hw1/main.c
@@ -4,13 +4,27 @@
 #include <time.h>
 #include <sys/time.h>
 
+#define DELAY_TIME 100000
+
 extern void random_length_task();
 
-int main() {
+int main(int argc, char* argv[]) {
 	int n = 10;
+	
+	if(argc == 2){
+		n = atoi(argv[1]);
+	}
+
 	srand(time(0) ^ getpid());
+	struct timeval origin, current;
 	while(n-- > 0) {
+		gettimeofday(&origin, NULL);
 		random_length_task();
+		gettimeofday(&current, NULL);
+
+		while(current.tv_usec - origin.tv_usec > DELAY_TIME) {
+			gettimeofday(&current, NULL);
+		}
 	}
 	return 0;
 }
