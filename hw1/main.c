838614addc35f222c7519e06923d044d65c21bf1
Date Huang <tjjh89017@hotmail.com>
2016-03-07 11:55:17 +0800

    update

--- hw1/main.c
+++ hw1/main.c
@@ -10,22 +10,25 @@ extern void random_length_task();
 
 int main(int argc, char* argv[]) {
 	int n = 10;
+	struct timeval origin;
+	struct timeval current;
 	
 	if(argc == 2){
 		n = atoi(argv[1]);
 	}
 
-	srand(time(0) ^ getpid());
-	struct timeval origin, current;
-	while(n-- > 0) {
+	srand(time(0));
+	while(n--){
 		gettimeofday(&origin, NULL);
 		random_length_task();
 		gettimeofday(&current, NULL);
 
-		while(current.tv_usec - origin.tv_usec > DELAY_TIME) {
+		while((1000000 + current.tv_usec - origin.tv_usec) % 1000000 < DELAY_TIME){
 			gettimeofday(&current, NULL);
 		}
+
 	}
+
 	return 0;
 }
 
